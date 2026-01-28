1dba8e3361d0840ed4f92b9e2fbbf45287a2be67
Isobe Kazuhiko <179515+muramasa64@users.noreply.github.com>
2026-01-28 15:26:02 +0900

    work端末のDockの設定を追加

--- hosts/work/default.nix
+++ hosts/work/default.nix
@@ -10,8 +10,15 @@
       { app = "/System/Applications/Calendar.app"; }
       { app = "/System/Applications/Reminders.app"; }
       { app = "/System/Applications/Home.app"; }
+      { app = "/Applications/Reeder.app"; }
       { app = "/Applications/Firefox.app"; }
+      { app = "/Applications/Firefox Developer Edition.app"; }
+      { app = "/Applications/Firefox Nightly.app"; }
+      { app = "/Applications/Evernote.app"; }
+      { app = "/Applications/Nani.app"; }
+      { app = "/Applications/Obsidian.app"; }
       { app = "/Applications/Ghostty.app"; }
+      { app = "/Applications/Zoom.app"; }
       { app = "/System/Applications/Messages.app"; }
       { app = "/System/Applications/Books.app"; }
     ];
