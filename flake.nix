4237f33e664d7e3b03a6aa7fbe512cb3bbb20ab0
Isobe Kazuhiko <179515+muramasa64@users.noreply.github.com>
2026-01-28 21:03:14 +0900

    starship-jj-pkgの定義を一箇所にまとめる

--- flake.nix
+++ flake.nix
@@ -31,31 +31,34 @@
 
       flake = let
         mkDarwinSystem = { hostname, username, system ? "aarch64-darwin" }:
-          inputs.nix-darwin.lib.darwinSystem {
-            inherit system;
-            specialArgs = {
-              inherit inputs hostname username;
-            };
+          let
+            starship-jj-pkg = inputs.starship-jj.packages.${system}.default;
+          in
+            inputs.nix-darwin.lib.darwinSystem {
+              inherit system;
+              specialArgs = {
+                inherit inputs hostname username starship-jj-pkg;
+              };
 
-            modules = [
-              ./modules/darwin/common.nix
-              ./hosts/${hostname}/default.nix
-              inputs.nix-homebrew.darwinModules.nix-homebrew
-              inputs.home-manager.darwinModules.home-manager
-              {
-                users.users.${username}.home = "/Users/${username}";
-                home-manager = {
-                  extraSpecialArgs = {
-                    inherit inputs hostname username;
-                    starship-jj-pkg = inputs.starship-jj.packages.${system}.default;
+              modules = [
+                ./modules/darwin/common.nix
+                ./hosts/${hostname}/default.nix
+                inputs.nix-homebrew.darwinModules.nix-homebrew
+                inputs.home-manager.darwinModules.home-manager
+                {
+                  users.users.${username}.home = "/Users/${username}";
+                  home-manager = {
+                    extraSpecialArgs = {
+                      inherit inputs hostname username starship-jj-pkg;
+                    };
+                    useGlobalPkgs = true;
+                    useUserPackages = true;
+                    backupFileExtension = "backup";
+                    users.${username} = import ./hosts/${hostname}/home.nix;
                   };
-                  useGlobalPkgs = true;
-                  useUserPackages = true;
-                  users.${username} = import ./hosts/${hostname}/home.nix;
-                };
-              }
-            ];
-          };
+                }
+              ];
+            };
       in {
         # sudo darwin-rebuild build --flake .#test
         darwinConfigurations = {
