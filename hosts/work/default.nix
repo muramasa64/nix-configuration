9d07860f97dafb55eddd8dfedc34160e914a43b1
Isobe Kazuhiko <179515+muramasa64@users.noreply.github.com>
2026-01-28 20:39:45 +0900

    homebrewの設定を移動

--- hosts/work/default.nix
+++ hosts/work/default.nix
@@ -23,4 +23,26 @@
       { app = "/System/Applications/Books.app"; }
     ];
   };
+
+  homebrew = {
+    taps = [
+      "nikitabobko/tap"
+      "pingidentity/tap"
+    ];
+    casks = [
+      "bruno"
+      "devtoys"
+      "firefox@developer-edition"
+      "firefox@nightly"
+      "microsoft-edge"
+      "orbstack"
+      "visual-studio-code"
+      "windows-app"
+      "xca"
+    ];
+    brews = [
+      "pingcli"
+      "resterm"
+    ];
+  };
 }
