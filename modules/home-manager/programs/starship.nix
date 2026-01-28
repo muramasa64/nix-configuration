c4cb1bc8f262e3503d3632aa52f1fe8f3a628ccb
Isobe Kazuhiko <179515+muramasa64@users.noreply.github.com>
2026-01-28 15:26:02 +0900

    starshipの設定を変更

--- modules/home-manager/programs/starship.nix
+++ modules/home-manager/programs/starship.nix
@@ -2,114 +2,301 @@
 {
   programs.starship = {
     enable = true;
-    enableFishIntegration = true;
-
     settings = {
-      format = "[░▒▓](#a3aed2)[  ](bg:#a3aed2 fg:#090c0c)[](bg:#769ff0 fg:#a3aed2)$directory[](fg:#769ff0 bg:#394260)\${custom.git_branch}\${custom.git_status}\${custom.starship-jj}[](fg:#394260 bg:#212736)$nodejs$rust$golang$php$ruby$python[](fg:#212736 bg:#1d2230)$time[](fg:#1d2230)\n\${env_var.VIRTUAL_ENV_PROMPT}\${env_var.DEVBOX_SHELL_ENABLED}\${env_var.SHELL}$character";
+      format = "[](red)$os$username[](bg:peach fg:red)$directory[](bg:yellow fg:peach)\${custom.git_branch}\${custom.git_status}\${custom.starship-jj}[](fg:yellow bg:green)$c$rust$golang$nodejs$php$java$kotlin$haskell$python[](fg:green bg:sapphire)$conda[](fg:sapphire bg:lavender)$time[ ](fg:lavender)$cmd_duration$line_break$character";
 
-      env_var = {
-        VIRTUAL_ENV_PROMPT = {
-          variable = "VIRTUAL_ENV_PROMPT";
-          format = "[($env_value)](fg:#a3aed2 bg:#1d2230) ";
-        };
+      palette = "catppuccin_mocha";
 
-        DEVBOX_SHELL_ENABLED = {
-          variable = "DEVBOX_SHELL_ENABLED";
-          format = "[(devbox)](fg:#a3aed2 bg:#1d2230) ";
-        };
+      character = {
+        disabled = false;
+        error_symbol = "[❯](bold fg:red)";
+        success_symbol = "[❯](bold fg:green)";
+        vimcmd_replace_one_symbol = "[❮](bold fg:lavender)";
+        vimcmd_replace_symbol = "[❮](bold fg:lavender)";
+        vimcmd_symbol = "[❮](bold fg:green)";
+        vimcmd_visual_symbol = "[❮](bold fg:yellow)";
+      };
 
-        SHELL = {
-          variable = "STARSHIP_SHELL";
-          format = "[$env_value](fg:#a3aed2 bg:#1d2230) ";
-          default = "";
+      os = {
+        disabled = false;
+        style = "bg:red fg:crust";
+        symbols = {
+          AOSC = "";
+          Alpine = "";
+          Amazon = "";
+          Android = "";
+          Arch = "󰣇";
+          Artix = "󰣇";
+          CentOS = "";
+          Debian = "󰣚";
+          Fedora = "󰣛";
+          Gentoo = "󰣨";
+          Linux = "󰌽";
+          Macos = "󰀵";
+          Manjaro = "";
+          Mint = "󰣭";
+          Raspbian = "󰐿";
+          RedHatEnterprise = "󱄛";
+          Redhat = "󱄛";
+          SUSE = "";
+          Ubuntu = "󰕈";
+          Windows = "";
         };
       };
+
+      username = {
+        format = "[ $user]($style)";
+        show_always = true;
+        style_root = "bg:red fg:crust";
+        style_user = "bg:red fg:crust";
+      };
+
 
       directory = {
-        style = "fg:#e3e5e5 bg:#769ff0";
         format = "[ $path ]($style)";
-        fish_style_pwd_dir_length = 1;
+        style = "bg:peach fg:crust";
         truncation_length = 3;
+        truncation_symbol = "…/";
       };
 
-      nodejs = {
-        symbol = "";
-        style = "bg:#212736";
-        format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+      custom.git_branch = {
+        command = "starship module git_branch | sd '.*\\s+(\\S+)\\s*.*' '$1'\n";
+        description = "Only show git_branch if we're not in a jj repo";
+        format = "[[ $symbol $output](fg:crust bg:yellow)]($style)";
+        style = "bg:yellow";
+        symbol = "";
+        when = "! jj --ignore-working-copy root";
+      };
+
+      custom.git_status = {
+        command = "starship module git_status | sed 's/\\x1b\\[[0-9;]*m//g'\n";
+        description = "Only show git_branch if we're not in a jj repo";
+        format = "[($output)(fg:crust bg:yellow)]($style)";
+        style = "bg:yellow";
+        when = "! jj --ignore-working-copy root";
+      };
+
+      custom.starship-jj = {
+        command = "prompt";
+        ignore_timeout = true;
+        shell = ["starship-jj" "--ignore-working-copy" "starship"];
+        use_stdin = false;
+        when = true;
+      };
+
+      c = {
+        format = "[[ $symbol( $version) ](fg:crust bg:green)]($style)";
+        style = "bg:green";
+        symbol = "";
       };
 
       deno = {
+        format = "[[ $symbol ($version) ](fg:crust bg:green)]($style)";
+        style = "bg:green";
         symbol = "🦕";
-        style = "bg:#212736";
-        format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
       };
 
-      rust = {
-        symbol = "";
-        style = "bg:#212736";
-        format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+      docker_context = {
+        format = "[[ $symbol( $context) ](fg:crust bg:sapphire)]($style)";
+        style = "bg:sapphire";
+        symbol = "";
       };
 
       golang = {
-        symbol = "";
-        style = "bg:#212736";
-        format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+        format = "[[ $symbol( $version) ](fg:crust bg:green)]($style)";
+        style = "bg:green";
+        symbol = "";
+      };
+
+      haskell = {
+        format = "[[ $symbol( $version) ](fg:crust bg:green)]($style)";
+        style = "bg:green";
+        symbol = "";
+      };
+
+      java = {
+        format = "[[ $symbol( $version) ](fg:crust bg:green)]($style)";
+        style = "bg:green";
+        symbol = "";
+      };
+
+      kotlin = {
+        format = "[[ $symbol( $version) ](fg:crust bg:green)]($style)";
+        style = "bg:green";
+        symbol = "";
+      };
+
+      nodejs = {
+        format = "[[ $symbol( $version) ](fg:crust bg:green)]($style)";
+        style = "bg:green";
+        symbol = "";
       };
 
       php = {
-        symbol = "";
-        style = "bg:#212736";
-        format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+        format = "[[ $symbol( $version) ](fg:crust bg:green)]($style)";
+        style = "bg:green";
+        symbol = "";
       };
 
       python = {
-        symbol = "";
-        style = "bg:#212736";
-        format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+        format = "[[ $symbol( $version)((#$virtualenv)) ](fg:crust bg:green)]($style)";
+        style = "bg:green";
+        symbol = "";
       };
 
       ruby = {
-        symbol = "";
-        style = "bg:#212736";
-        format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+        format = "[[ $symbol($ version)](fg:crust bg:green)]($style)";
+        style = "bg:green";
+        symbol = "";
+      };
+
+      rust = {
+        format = "[[ $symbol( $version) ](fg:crust bg:green)]($style)";
+        style = "bg:green";
+        symbol = "";
+      };
+
+      conda = {
+        format = "[$symbol$environment ]($style)";
+        ignore_base = false;
+        style = "fg:crust bg:sapphire";
+        symbol = "  ";
       };
 
       time = {
         disabled = false;
+        format = "[[  $time ](fg:crust bg:lavender)]($style)";
+        style = "bg:lavender";
         time_format = "%R";
-        style = "bg:#1d2230";
-        format = "[[  $time ](fg:#a0a9cb bg:#1d2230)]($style)";
       };
 
-      custom = {
-        starship-jj = {
-          command = "prompt";
-          ignore_timeout = true;
-          shell = ["starship-jj" "--ignore-working-copy" "starship"];
-          use_stdin = false;
-          when = true;
-        };
+      cmd_duration = {
+        disabled = false;
+        format = " in $duration ";
+        min_time_to_notify = 45000;
+        show_milliseconds = true;
+        show_notifications = true;
+        style = "bg:lavender";
+      };
 
-        git_branch = {
-          when = "! jj --ignore-working-copy root";
-          command = ''
-            starship module git_branch | sd '.*\s+(\S+)\s*.*' '$1'
-          '';
-          description = "Only show git_branch if we're not in a jj repo";
-          symbol = "";
-          style = "bg:#394260";
-          format = "[[ $symbol $output](fg:#769ff0 bg:#394260)]($style)";
-        };
+      # line_break = {
+      #   disable = true;
+      # };
 
-        git_status = {
-          when = "! jj --ignore-working-copy root";
-          command = ''
-            starship module git_status | sed 's/\x1b\[[0-9;]*m//g'
-          '';
-          description = "Only show git_branch if we're not in a jj repo";
-          style = "bg:#394260";
-          format = "[($output)(fg:#769ff0 bg:#394260)]($style)";
-        };
+      palettes.catppuccin_frappe = {
+        base = "#303446";
+        blue = "#8caaee";
+        crust = "#232634";
+        flamingo = "#eebebe";
+        green = "#a6d189";
+        lavender = "#babbf1";
+        mantle = "#292c3c";
+        maroon = "#ea999c";
+        mauve = "#ca9ee6";
+        overlay0 = "#737994";
+        overlay1 = "#838ba7";
+        overlay2 = "#949cbb";
+        peach = "#ef9f76";
+        pink = "#f4b8e4";
+        red = "#e78284";
+        rosewater = "#f2d5cf";
+        sapphire = "#85c1dc";
+        sky = "#99d1db";
+        subtext0 = "#a5adce";
+        subtext1 = "#b5bfe2";
+        surface0 = "#414559";
+        surface1 = "#51576d";
+        surface2 = "#626880";
+        teal = "#81c8be";
+        text = "#c6d0f5";
+        yellow = "#e5c890";
+      };
+
+      palettes.catppuccin_latte = {
+        base = "#eff1f5";
+        blue = "#1e66f5";
+        crust = "#dce0e8";
+        flamingo = "#dd7878";
+        green = "#40a02b";
+        lavender = "#7287fd";
+        mantle = "#e6e9ef";
+        maroon = "#e64553";
+        mauve = "#8839ef";
+        overlay0 = "#9ca0b0";
+        overlay1 = "#8c8fa1";
+        overlay2 = "#7c7f93";
+        peach = "#fe640b";
+        pink = "#ea76cb";
+        red = "#d20f39";
+        rosewater = "#dc8a78";
+        sapphire = "#209fb5";
+        sky = "#04a5e5";
+        subtext0 = "#6c6f85";
+        subtext1 = "#5c5f77";
+        surface0 = "#ccd0da";
+        surface1 = "#bcc0cc";
+        surface2 = "#acb0be";
+        teal = "#179299";
+        text = "#4c4f69";
+        yellow = "#df8e1d";
+      };
+
+      palettes.catppuccin_macchiato = {
+        base = "#24273a";
+        blue = "#8aadf4";
+        crust = "#181926";
+        flamingo = "#f0c6c6";
+        green = "#a6da95";
+        lavender = "#b7bdf8";
+        mantle = "#1e2030";
+        maroon = "#ee99a0";
+        mauve = "#c6a0f6";
+        overlay0 = "#6e738d";
+        overlay1 = "#8087a2";
+        overlay2 = "#939ab7";
+        peach = "#f5a97f";
+        pink = "#f5bde6";
+        red = "#ed8796";
+        rosewater = "#f4dbd6";
+        sapphire = "#7dc4e4";
+        sky = "#91d7e3";
+        subtext0 = "#a5adcb";
+        subtext1 = "#b8c0e0";
+        surface0 = "#363a4f";
+        surface1 = "#494d64";
+        surface2 = "#5b6078";
+        teal = "#8bd5ca";
+        text = "#cad3f5";
+        yellow = "#eed49f";
+      };
+
+      palettes.catppuccin_mocha = {
+        base = "#1e1e2e";
+        blue = "#89b4fa";
+        crust = "#11111b";
+        flamingo = "#f2cdcd";
+        green = "#a6e3a1";
+        lavender = "#b4befe";
+        mantle = "#181825";
+        maroon = "#eba0ac";
+        mauve = "#cba6f7";
+        overlay0 = "#6c7086";
+        overlay1 = "#7f849c";
+        overlay2 = "#9399b2";
+        peach = "#fab387";
+        pink = "#f5c2e7";
+        red = "#f38ba8";
+        rosewater = "#f5e0dc";
+        sapphire = "#74c7ec";
+        sky = "#89dceb";
+        subtext0 = "#a6adc8";
+        subtext1 = "#bac2de";
+        surface0 = "#313244";
+        surface1 = "#45475a";
+        surface2 = "#585b70";
+        teal = "#94e2d5";
+        text = "#cdd6f4";
+        yellow = "#f9e2af";
       };
     };
   };
