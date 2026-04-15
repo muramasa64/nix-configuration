4bb8136eb95640f534dc8e0fcb762f4530104f3d
Isobe Kazuhiko <179515+muramasa64@users.noreply.github.com>
2026-04-15 11:40:01 +0900

    masAppsでMac App Storeのアプリのインストールができなくなったのでコメントを追記して無効化

--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -19,11 +19,17 @@
       "nani"
       "menumeters"
     ];
-    masApps = {
-      "Reeder Classic." = 1529448980;
-      "OmniFocus 4" = 1542143627;
-      "Snip : Snippets Manager" = 1527428847;
-      "辞書 by 物書堂" = 1380563956;
-    };
+    # masAppsでインストールできなくなったっぽい
+    # 1. mas 6.x で signin コマンドが削除された
+    # mas 6.0.1 は CLI でのサインインを提供しておらず、App Store.app で GUI ログインしている状態を前提としている
+    # brew bundle が root として実行される
+    # sudo nix run nix-darwin -- switch で動かすため、brew bundle が root コンテキストで実行される
+    # root は App Store のユーザーセッションにアクセスできないため、mas install が失敗する
+    # masApps = {
+    #   "Reeder Classic." = 1529448980;
+    #   "OmniFocus 4" = 1542143627;
+    #   "Snip : Snippets Manager" = 1527428847;
+    #   "辞書 by 物書堂" = 1380563956;
+    # };
   };
 }
