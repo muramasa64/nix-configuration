4f6e8a2b78026cdc9c2bf9080502fdb782089252
Isobe Kazuhiko <179515+muramasa64@users.noreply.github.com>
2026-06-18 17:47:47 +0900

    nix-envは不要なので削除

--- modules/home-manager/programs/fish.nix
+++ modules/home-manager/programs/fish.nix
@@ -109,15 +109,6 @@
           sha256 = "sha256-4+k5rSoxkTtYFh/lEjhRkVYa2S4KEzJ/IJbyJl+rJjQ=";
         };
       }
-      {
-        name = "nix-env.fish";
-        src = pkgs.fetchFromGitHub {
-          owner = "lilyball";
-          repo = "nix-env.fish";
-          rev = "7b65bd228429e852c8fdfa07601159130a818cfa";
-          sha256 = "sha256-RG/0rfhgq6aEKNZ0XwIqOaZ6K5S4+/Y5EEMnIdtfPhk=";
-        };
-      }
     ];
   };
 }
